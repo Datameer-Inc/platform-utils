cfe67e0c145339c9dac89aed23f857869e1f1d8c
Steve Boardwell <steve.boardwell@datameer.com>
2021-01-28 14:24:31 +0100

    Install ansible, Makefile, tweaks

--- init.sh
+++ init.sh
@@ -4,60 +4,56 @@
 
 set -euo pipefail
 
-repo_name='spotlight-utils'
-repo_url="https://github.com/Datameer-Inc/${repo_name}"
+repo_url='https://github.com/Datameer-Inc/spotlight-utils'
 latest_url="${repo_url}/releases/latest"
-default_path="/tmp/${repo_name}"
-base_path="${UTILS_INSTALL_PATH:-${default_path}}"
+root_dir="${PU_BASE_INSTALL_DIR:-/tmp}"
 
-die() {
+function die() {
   echo "[$(date +'%Y-%m-%dT%H:%M:%S%z')]: $*" >&2
   exit 1
 }
 
-info() {
+function info() {
   echo "[$(date +'%Y-%m-%dT%H:%M:%S%z')]: $*"
 }
 
-determine_latest_version() {
-    if ! latest_release_url=$(curl -fsSLI -o /dev/null -w %{url_effective} "${latest_url}"); then
-        die "Problem getting ${latest_url}"
-    fi
-    if [[ $latest_release_url =~ releases/tag ]]; then
-        release="${latest_release_url//*\//}"
-    else
-        release='master'
-    fi
-    download_file="${release}.tar.gz"
-    download_url="${repo_url}/archive/${download_file}"
-    install_path="${base_path}/${release}"
-    latest_path="${base_path}/latest"
+function download_and_extract() {
+  if ! latest_release_url=$(curl -fsSLI -o /dev/null -w %{url_effective} "${latest_url}"); then
+    die "Problem getting ${latest_url}"
+  fi
+  if [[ $latest_release_url =~ releases/tag ]]; then
+    release="${latest_release_url//*\//}"
+  else
+    release='master'
+  fi
+  download_file="${release}.tar.gz"
+  download_url="${repo_url}/archive/${download_file}"
+  install_path="${root_dir}/spotlight-utils/${release}"
+  latest_path="${root_dir}/spotlight-utils/latest"
+  if [ -d "${install_path}" ]; then
+    info "Install path already exists. No need to install..."
+  elif [ -e "${install_path}" ]; then
+    die "Install path already exists but not a directory."
+  else
+    info "Deleting any previous installations at '$(dirname ${install_path})'"
+    rm -rf "$(dirname ${install_path})"
+    info "Downloading '${download_url}' to directory '${install_path}'"
+    mkdir -p "${install_path}"
+    curl -fsSL -o "${install_path}/${download_file}" "${download_url}"
+    cd "${install_path}"
+    info "Extracting '${download_file}'"
+    tar xzf "${download_file}" --strip 1
+    cd -
+    info "Setting symlink '${install_path}' -> '${latest_path}'"
+    rm -f "${latest_path}"
+    ln -sf "${install_path}" "${latest_path}"
+  fi
 }
 
-download_and_extract() {
-    if [ -d "${install_path}" ]; then
-        info "Install path already exists. No need to install..."
-    elif [ -e "${install_path}" ]; then
-        die "Install path already exists but not a directory."
-    else
-        info "Downloading '${download_url}' to directory '${install_path}'"
-        mkdir -p "${install_path}"
-        curl -fsSL -o "${install_path}/${download_file}" "${download_url}"
-        pushd "${install_path}"
-            info "Extracting '${download_file}'"
-            tar xzf "${download_file}" --strip 1
-        popd
-        info "Setting symlink '${install_path}' -> '${latest_path}'"
-        rm -f "${latest_path}"
-        ln -sf "${install_path}" "${latest_path}"
-    fi
-}
-
-determine_latest_version
 download_and_extract
 tools_script="${latest_path}/process-tools.sh"
 if [ ! -f "${tools_script}" ]; then
-    die "Could not find ${tools_script}"
+  die "Could not find ${tools_script}"
 else
-    "${tools_script}"
+  "${tools_script}"
 fi
--- process-tools.sh
+++ process-tools.sh
@@ -2,4 +2,19 @@
 
 set -euo pipefail
 
-echo "Processing tools....yippee"
+script_dir="$( cd "$( dirname "${BASH_SOURCE[0]:-}" )" >/dev/null 2>&1 && pwd )"
+source "${script_dir}/functions.sh"
+
+PU_TOOLS_ROOT="${script_dir}/tools"
+
+process_tools() {
+  local tool_name
+  printf "Processing tools..."
+  for tool_dir in $(find "${PU_TOOLS_ROOT}" -maxdepth 1 -type d); do
+    tool_name=$(basename "${tool_dir}")
+    if [ -x "${PU_TOOLS_ROOT}/${tool_name}/process.sh" ]; then
+      "${PU_TOOLS_ROOT}/${tool_name}/process.sh"
+    fi
+  done
+}
+process_tools
