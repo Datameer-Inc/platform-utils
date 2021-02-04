ddc353c2b202d3d187f5337a8d7eb880683abc88
Steve Boardwell <steve.boardwell@datameer.com>
2021-02-04 19:41:29 +0100

    fixup: use underscores to separate cluster id and name

--- tools/datadog/components/emr/core-node.sh
+++ tools/datadog/components/emr/core-node.sh
@@ -1,5 +1,5 @@
 init_instance_tags
 CLUSTER_ID=$(get_instance_tag 'aws:elasticmapreduce:job-flow-id')
 CLUSTER_NAME=$(aws emr describe-cluster --cluster-id $CLUSTER_ID --region $AWS_REGION --query 'Cluster.Name' --output text)
-DD_CLUSTER_NAME="${CLUSTER_NAME}-${CLUSTER_ID}"
+DD_CLUSTER_NAME="${CLUSTER_NAME}_${CLUSTER_ID}"
 HDFS_DATANODE_JMX_URI="http://$(hostname -f):9864"
